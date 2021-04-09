f2b450d209a36b2fc5a97409c6392aa8ef59c83d
unsivil <unsivilaudio@gmail.com>
2021-04-09 10:25:59 -0600

    some styling tweaks

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -10,15 +10,24 @@ interface CellListItemProps {
 
 const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
     let child: JSX.Element;
-    if (cell.type === 'code') child = <CodeCell cell={cell} />;
-    else child = <TextEditor cell={cell} />;
+    if (cell.type === 'code') {
+        child = (
+            <>
+                <div className='action-bar-wrapper'>
+                    <ActionBar id={cell.id} />
+                </div>
+                <CodeCell cell={cell} />
+            </>
+        );
+    } else
+        child = (
+            <>
+                <TextEditor cell={cell} />
+                <ActionBar id={cell.id} />
+            </>
+        );
 
-    return (
-        <div className='cell-list-item'>
-            <ActionBar id={cell.id} />
-            {child}
-        </div>
-    );
+    return <div className='cell-list-item'>{child}</div>;
 };
 
 export default CellListItem;
